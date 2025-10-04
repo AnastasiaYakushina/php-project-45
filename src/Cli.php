4ce6907d045a6d32d5b62a1d4b3350d2d442b225
AnastasiaYakushina <aubarr@ya.ru>
2025-10-04 22:39:15 +0300

    Add linter and badges

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,9 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greet() {
+function greet()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);   
-};
-
+    line("Hello, %s!", $name);
+}
