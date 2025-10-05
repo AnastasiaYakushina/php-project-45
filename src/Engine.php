6f6fc75ec9e0c0e2572a4b5e31b16a592548f63c
AnastasiaYakushina <aubarr@ya.ru>
2025-10-05 23:42:03 +0300

    Add game "Prime"

--- src/Engine.php
+++ src/Engine.php
@@ -9,6 +9,7 @@ use function BrainGames\Games\Even\generateEvenTask;
 use function BrainGames\Games\Calc\generateCalcTask;
 use function BrainGames\Games\Gcd\generateGcdTask;
 use function BrainGames\Games\Progression\generateProgressionTask;
+use function BrainGames\Games\Prime\generatePrimeTask;
 
 const ROUNDS_COUNT = 3;
 const MIN_NUMBER = 1;
@@ -29,6 +30,9 @@ function generateTask($gameName)
         case 'progression':
             return generateProgressionTask(MIN_NUMBER, MAX_NUMBER);
             break;
+        case 'prime':
+            return generatePrimeTask(MIN_NUMBER, MAX_NUMBER);
+            break;
     }
 }
 
@@ -36,7 +40,7 @@ function startGame($gameName, $question)
 {
     $name = greet();
     line($question);
-    for ($round = 1, $taskIndex = 0; $round <= ROUNDS_COUNT; $round += 1, $taskIndex += 1) {
+    for ($round = 1; $round <= ROUNDS_COUNT; $round += 1) {
         [$task, $correctAnswer] = generateTask($gameName);
         line("Question: {$task}");
         $userAnswer = prompt('Your answer');
---
+++ src/Games/Prime.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BrainGames\Games\Prime;
+
+use function BrainGames\Engine\startGame;
+
+function isPrime($number)
+{
+    if ($number <= 1) {
+        return 'no';
+    }
+    for ($i = 2; $i < $number; $i += 1) {
+        if ($number % $i === 0) {
+            return 'no';
+        }
+    }
+    return 'yes';
+}
+
+function generatePrimeTask($minNumber, $maxNumber)
+{
+    $task = rand($minNumber, $maxNumber);
+    $answer = isPrime($task);
+    return [$task, $answer];
+}
+
+function playPrime()
+{
+    $question = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    startGame('prime', $question);
+}
